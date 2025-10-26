476ae90f08560f517452d377d490ba5ae09e9d73
LucasLaza <110206068+LucasLaza@users.noreply.github.com>
2025-10-26 18:57:35 -0300

    Adding Mochawesome Report integration

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,7 @@
 import './commands'
 import 'cypress-file-upload'
+// register mochawesome reporter (adds screenshots metadata to results)
+import 'cypress-mochawesome-reporter/register'
 
 // Optionally set up global hooks
 Cypress.on('uncaught:exception', (err, runnable) => {
